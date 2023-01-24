6d78596dfb86b06122730f6b70da4019c651b198
RasmusKibshedeMeew <rasmus.kibshede@meew.dk>
2023-01-24 16:35:05 +0100

    added antD to pokemon view

--- src/ui/pages/tabs/PokemonPage/PokemonPage.tsx
+++ src/ui/pages/tabs/PokemonPage/PokemonPage.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { IonContent, IonTitle } from '@ionic/react';
+import { Divider, List, Typography } from 'antd';
+import { Pokemon } from 'types/data-types-import';
+import { supabase } from 'apis/supabaseClient';
 
-const Pokemon: React.FC = () => (
-  <IonContent color={'white-background'}>
-    <IonTitle>Pokemon</IonTitle>
-    
-  </IonContent>
-);
+const PokemonPage: React.FC = () => {
 
-export default Pokemon;
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  async function fetchPokemon() {
+    const { data, error } = await supabase.from('pokemon').select();
+
+    if (data) {
+      setPokemon(data);
+    };
+  };
+
+  return (
+
+    <IonContent color={'white-background'}>
+
+      <>
+        <List
+          size="large"
+          header={<h2>Pokemon</h2>}
+          bordered
+          dataSource={pokemon}
+          renderItem={(item) => <List.Item><List.Item.Meta title={item.name} description={item.description}></List.Item.Meta></List.Item>}
+        />
+      </>
+
+
+    </IonContent>
+  );
+};
+
+export default PokemonPage;
