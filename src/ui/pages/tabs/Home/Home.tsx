bfe90329735cc03cef171eedc342b484c9906992
RasmusKibshedeMeew <rasmus.kibshede@meew.dk>
2023-01-25 14:31:10 +0100

    added egde function fetch

--- src/ui/pages/tabs/Home/Home.tsx
+++ src/ui/pages/tabs/Home/Home.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLoggedInUser } from 'store/loggedInUser';
 import { IonContent, IonTitle } from '@ionic/react';
-import TakePicture from 'ui/components/frontpage/take-picture/TakePicture';
-import { Photo } from '@capacitor/camera';
+import { supabase } from 'apis/supabaseClient';
 
 const Home: React.FC = () => {
-  // const [picture, setPicture] = React.useState<Photo>();
+  const [message, setMessage] = useState('');
+
+  const user = useLoggedInUser();
+
+
+  async function egde() {
+
+    const { data, error } = await supabase.functions.invoke('hello', {
+      body: { name: user.user?.first_name },
+    });
+
+    if (data) {
+      setMessage(data.message);
+    };
+
+  };
+
+  useEffect(() => {
+    egde();
+  }, []);
+
+
 
   return (
     <IonContent color={'white-background'}>
 
-      <h1 className='text-center'>Welcome</h1>
+      <h1 className='text-center'>{message}</h1>
 
 
     </IonContent>
